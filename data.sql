0af473d1174f5f13c0284109d1a5d952ce4226ba
Anuar <anuar31.08.25@gmail.com>
2026-04-16 21:56:55 +0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -21,3 +21,42 @@ INSERT INTO medicine_prices (city_id, medicine_id, price, price_date) VALUES
 (1, 3, 300, '2024-01-01'),
 (2, 3, 350, '2024-01-01'),
 (3, 3, 280, '2024-01-01');
+-- Добавляем города
+INSERT INTO cities (name) VALUES
+('Karaganda'),
+('Aktobe'),
+('Taraz'),
+('Pavlodar'),
+('Ust-Kamenogorsk'),
+('Semey'),
+('Atyrau'),
+('Kostanay'),
+('Kyzylorda');
+
+-- Добавляем лекарства
+INSERT INTO medicines (name) VALUES
+('Amoxicillin'),
+('Ceftriaxone'),
+('Nurofen'),
+('Analgin'),
+('No-shpa'),
+('Citramon'),
+('Ambroxol'),
+('Loratadine'),
+('Omeprazole'),
+('Panadol');
+
+-- Добавляем цены
+INSERT INTO medicine_prices (city_id, medicine_id, price, price_date) VALUES
+(1, 1, 520, '2024-02-01'),
+(1, 1, 580, '2024-03-01'),
+(2, 1, 610, '2024-02-01'),
+(2, 1, 630, '2024-03-01'),
+
+(1, 4, 900, '2024-01-01'),
+(2, 4, 950, '2024-01-01'),
+(3, 4, 870, '2024-01-01'),
+
+(1, 5, 1200, '2024-01-01'),
+(2, 5, 1150, '2024-01-01'),
+(3, 5, 1100, '2024-01-01');
