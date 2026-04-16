1ede8868617948100d2c06a0e9b08ee891c5cc47
Anuar <anuar31.08.25@gmail.com>
2026-04-16 22:00:35 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -18,3 +18,15 @@ FROM medicine_prices;
 
 -- Проверка индекса
 EXPLAIN SELECT * FROM medicine_prices WHERE medicine_id = 1;
+-- Где цена выше средней по лекарству (анализ отклонений)
+SELECT
+    m.name AS medicine_name,
+    c.name AS city_name,
+    p.price,
+    ROUND(AVG(p.price) OVER (PARTITION BY m.id), 2) AS avg_price,
+    ROUND(p.price - AVG(p.price) OVER (PARTITION BY m.id), 2) AS diff_from_avg
+FROM medicine_prices p
+JOIN medicines m ON p.medicine_id = m.id
+JOIN cities c ON p.city_id = c.id
+WHERE p.price > AVG(p.price) OVER (PARTITION BY m.id)
+ORDER BY diff_from_avg DESC;
